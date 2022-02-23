6862e29f4b6ba6666798f15988ca41c097cf2a66
avikhanukaev <avi.k@duda.co>
2022-02-23 12:50:34 +0200

    Create spring project

---
+++ src/main/java/com/amigos/java/spring/course/amigoscourse/AmigosCourseApplication.java
@@ -0,0 +1,13 @@
+package com.amigos.java.spring.course.amigoscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amigos/java/spring/course/amigoscourse/AmigosCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigos.java.spring.course.amigoscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
